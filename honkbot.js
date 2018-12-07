ef9f58b835e13d049e267c2ac61dd0721d4954bd
Noah Kindervag <noahk@protonmail.com>
2018-12-07 11:39:21 -0600

    Update honkbot.js

--- honkbot.js
+++ honkbot.js
@@ -21,7 +21,7 @@ bot.on('ready', function (evt) {
 });
 
 // Get Emojis
-const emojiList = bot.guild.emojis.map(e=>e.toString()).join(" ");
+const emojiList = bot.emojis.map(e=>e.toString()).join(" ");
 
 // Listener for responding to chat messages
 bot.on('message', function (user, userID, channelID, message, evt) {
