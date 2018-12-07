d01e4d4a343f2283feaf8291a6dbed174953e2b4
Noah Kindervag <noahk@protonmail.com>
2018-12-07 12:09:59 -0600

    holy shit this is bad

--- honkbot.js
+++ honkbot.js
@@ -1,132 +1,136 @@
-let Discord = require('discord.io');
-let logger = require('winston');
-let auth = require('./auth.json');
+main()
 
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
+async function main() { 
+    let Discord = require('discord.io');
+    let logger = require('winston');
+    let auth = require('./auth.json');
 
-// Initialize Discord Bot
-let bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
+    // Configure logger settings
+    logger.remove(logger.transports.Console);
+    logger.add(logger.transports.Console, {
+        colorize: true
+    });
+    logger.level = 'debug';
 
-await sleep(3000) // this is awful
+    // Initialize Discord Bot
+    let bot = new Discord.Client({
+    token: auth.token,
+    autorun: true
+    });
+    bot.on('ready', function (evt) {
+        logger.info('Connected');
+        logger.info('Logged in as: ');
+        logger.info(bot.username + ' - (' + bot.id + ')');
+    });
 
-// Get Emojis
-const coolbob = bot.emojis.find(emoji => emoji.name === "coolbob");
-const bill = bot.emojis.find(emoji => emoji.name === "bill");
-const dan = bot.emojis.find(emoji => emoji.name === "dan");
-const jacob = bot.emojis.find(emoji => emoji.name === "jacob");
-const jake = bot.emojis.find(emoji => emoji.name === "jake");
-const kelly = bot.emojis.find(emoji => emoji.name === "kelly");
-const kyle = bot.emojis.find(emoji => emoji.name === "kyle");
-const noah = bot.emojis.find(emoji => emoji.name === "noah");
-const yetinate = bot.emojis.find(emoji => emoji.name === "yetinate");
-const james = bot.emojis.find(emoji => emoji.name === "james");
-const kaleb = bot.emojis.find(emoji => emoji.name === "kaleb");
+    await sleep(3000) // this is awful
 
-// Listener for responding to chat messages
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Commands that start with "!"
-    if (message.substring(0, 1) == '!') {
-        let args = message.substring(1).split(' ');
-        let cmd = args[0];
+    // Get Emojis
+    const coolbob = bot.emojis.find(emoji => emoji.name === "coolbob");
+    const bill = bot.emojis.find(emoji => emoji.name === "bill");
+    const dan = bot.emojis.find(emoji => emoji.name === "dan");
+    const jacob = bot.emojis.find(emoji => emoji.name === "jacob");
+    const jake = bot.emojis.find(emoji => emoji.name === "jake");
+    const kelly = bot.emojis.find(emoji => emoji.name === "kelly");
+    const kyle = bot.emojis.find(emoji => emoji.name === "kyle");
+    const noah = bot.emojis.find(emoji => emoji.name === "noah");
+    const yetinate = bot.emojis.find(emoji => emoji.name === "yetinate");
+    const james = bot.emojis.find(emoji => emoji.name === "james");
+    const kaleb = bot.emojis.find(emoji => emoji.name === "kaleb");
 
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
+    // Listener for responding to chat messages
+    bot.on('message', function (user, userID, channelID, message, evt) {
+        // Commands that start with "!"
+        if (message.substring(0, 1) == '!') {
+            let args = message.substring(1).split(' ');
+            let cmd = args[0];
 
-            // !gamble
-            case 'gamble':
-                if (user == "KyloRennington") {
-                    // get beaned kyle
+            args = args.splice(1);
+            switch(cmd) {
+                // !ping
+                case 'ping':
                     bot.sendMessage({
                         to: channelID,
-                        message: "you lose"
+                        message: 'Pong!'
                     });
-                } else {
+                break;
+
+                // !gamble
+                case 'gamble':
+                    if (user == "KyloRennington") {
+                        // get beaned kyle
+                        bot.sendMessage({
+                            to: channelID,
+                            message: "you lose"
+                        });
+                    } else {
+                        bot.sendMessage({
+                            to: channelID,
+                            message: "you win"
+                        });
+                    }
+                break;
+
+                // !northerngeese posts northen geese faces
+                case 'northerngeese':
                     bot.sendMessage({
                         to: channelID,
-                        message: "you win"
+                        message: "${yetinate} ${james}: ${kaleb}"
                     });
-                }
-            break;
+                break;
 
-            // !northerngeese posts northen geese faces
-            case 'northerngeese':
+                // !southerngeese posts southern geese faces
+                case 'southerngeese':
                 bot.sendMessage({
                     to: channelID,
-                    message: "${yetinate} ${james}: ${kaleb}"
+                    message: "${coolbob} ${bill} ${dan} ${jacob} ${jake} ${kelly} ${kyle} ${noah}"
                 });
             break;
+            }
+        }
 
-            // !southerngeese posts southern geese faces
-            case 'southerngeese':
+        // Responds to "honk" with "honk". This is the most important feature.
+        if (message.toLowerCase().includes("honk") && user != "honkbot") {
             bot.sendMessage({
                 to: channelID,
-                message: "${coolbob} ${bill} ${dan} ${jacob} ${jake} ${kelly} ${kyle} ${noah}"
+                message: "honk"
             });
-        break;
         }
-     }
 
-     // Responds to "honk" with "honk". This is the most important feature.
-     if (message.toLowerCase().includes("honk") && user != "honkbot") {
-         bot.sendMessage({
-             to: channelID,
-             message: "honk"
-         });
-     }
+        // Also honkbot hanks now
+        if (message.toLowerCase().includes("hank") && user != "honkbot") {
+            bot.sendMessage({
+                to: channelID,
+                message: "hank"
+            });
+        }
 
-     // Also honkbot hanks now
-     if (message.toLowerCase().includes("hank") && user != "honkbot") {
-        bot.sendMessage({
-            to: channelID,
-            message: "hank"
-        });
-    }
-
-    /* Fuck Strodl Bot
-    Does the mocking spongebob text thing to whatever Strodl Bot says. I should extend this to include other users.
-    If there is no text to mock, simply tells Strodl Bot to fuck off. */
-     if (user == "Strodl Bot") {
-        let mocking_message = ""
-        let sufficiently_mocked = false
-        for (let i = 0; i < message.length; i++) {
-            letter = message.charAt(i)
-            if (Math.random() <= 0.5) {
-                letter = letter.toUpperCase()
-                sufficiently_mocked = true
+        /* Fuck Strodl Bot
+        Does the mocking spongebob text thing to whatever Strodl Bot says. I should extend this to include other users.
+        If there is no text to mock, simply tells Strodl Bot to fuck off. */
+        if (user == "Strodl Bot") {
+            let mocking_message = ""
+            let sufficiently_mocked = false
+            for (let i = 0; i < message.length; i++) {
+                letter = message.charAt(i)
+                if (Math.random() <= 0.5) {
+                    letter = letter.toUpperCase()
+                    sufficiently_mocked = true
+                }
+                mocking_message += letter
             }
-            mocking_message += letter
+            if (mocking_message == "" || sufficiently_mocked == false) {
+                mocking_message = "fuck u Strodl Bot"
+            }
+            bot.sendMessage({
+                to: channelID,
+                message: mocking_message
+            });
         }
-        if (mocking_message == "" || sufficiently_mocked == false) {
-            mocking_message = "fuck u Strodl Bot"
-        }
-        bot.sendMessage({
-            to: channelID,
-            message: mocking_message
-        });
-     }
-});
+    });
+}
 
 // lmao
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
