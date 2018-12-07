d2cb7659f8ac54175c8e08873e38420ac6f685e0
Noah Kindervag <noahk@protonmail.com>
2018-12-07 12:13:30 -0600

    i give up

--- honkbot.js
+++ honkbot.js
@@ -1,136 +1,112 @@
-main()
+let Discord = require('discord.io');
+let logger = require('winston');
+let auth = require('./auth.json');
 
-async function main() { 
-    let Discord = require('discord.io');
-    let logger = require('winston');
-    let auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
 
-    // Configure logger settings
-    logger.remove(logger.transports.Console);
-    logger.add(logger.transports.Console, {
-        colorize: true
-    });
-    logger.level = 'debug';
+// Initialize Discord Bot
+let bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
 
-    // Initialize Discord Bot
-    let bot = new Discord.Client({
-    token: auth.token,
-    autorun: true
-    });
-    bot.on('ready', function (evt) {
-        logger.info('Connected');
-        logger.info('Logged in as: ');
-        logger.info(bot.username + ' - (' + bot.id + ')');
-    });
+// Listener for responding to chat messages
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Commands that start with "!"
+    if (message.substring(0, 1) == '!') {
+        let args = message.substring(1).split(' ');
+        let cmd = args[0];
 
-    await sleep(3000) // this is awful
-
-    // Get Emojis
-    const coolbob = bot.emojis.find(emoji => emoji.name === "coolbob");
-    const bill = bot.emojis.find(emoji => emoji.name === "bill");
-    const dan = bot.emojis.find(emoji => emoji.name === "dan");
-    const jacob = bot.emojis.find(emoji => emoji.name === "jacob");
-    const jake = bot.emojis.find(emoji => emoji.name === "jake");
-    const kelly = bot.emojis.find(emoji => emoji.name === "kelly");
-    const kyle = bot.emojis.find(emoji => emoji.name === "kyle");
-    const noah = bot.emojis.find(emoji => emoji.name === "noah");
-    const yetinate = bot.emojis.find(emoji => emoji.name === "yetinate");
-    const james = bot.emojis.find(emoji => emoji.name === "james");
-    const kaleb = bot.emojis.find(emoji => emoji.name === "kaleb");
-
-    // Listener for responding to chat messages
-    bot.on('message', function (user, userID, channelID, message, evt) {
-        // Commands that start with "!"
-        if (message.substring(0, 1) == '!') {
-            let args = message.substring(1).split(' ');
-            let cmd = args[0];
-
-            args = args.splice(1);
-            switch(cmd) {
-                // !ping
-                case 'ping':
-                    bot.sendMessage({
-                        to: channelID,
-                        message: 'Pong!'
-                    });
-                break;
-
-                // !gamble
-                case 'gamble':
-                    if (user == "KyloRennington") {
-                        // get beaned kyle
-                        bot.sendMessage({
-                            to: channelID,
-                            message: "you lose"
-                        });
-                    } else {
-                        bot.sendMessage({
-                            to: channelID,
-                            message: "you win"
-                        });
-                    }
-                break;
-
-                // !northerngeese posts northen geese faces
-                case 'northerngeese':
-                    bot.sendMessage({
-                        to: channelID,
-                        message: "${yetinate} ${james}: ${kaleb}"
-                    });
-                break;
-
-                // !southerngeese posts southern geese faces
-                case 'southerngeese':
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
                 bot.sendMessage({
                     to: channelID,
-                    message: "${coolbob} ${bill} ${dan} ${jacob} ${jake} ${kelly} ${kyle} ${noah}"
+                    message: 'Pong!'
                 });
             break;
-            }
-        }
 
-        // Responds to "honk" with "honk". This is the most important feature.
-        if (message.toLowerCase().includes("honk") && user != "honkbot") {
-            bot.sendMessage({
-                to: channelID,
-                message: "honk"
-            });
-        }
-
-        // Also honkbot hanks now
-        if (message.toLowerCase().includes("hank") && user != "honkbot") {
-            bot.sendMessage({
-                to: channelID,
-                message: "hank"
-            });
-        }
-
-        /* Fuck Strodl Bot
-        Does the mocking spongebob text thing to whatever Strodl Bot says. I should extend this to include other users.
-        If there is no text to mock, simply tells Strodl Bot to fuck off. */
-        if (user == "Strodl Bot") {
-            let mocking_message = ""
-            let sufficiently_mocked = false
-            for (let i = 0; i < message.length; i++) {
-                letter = message.charAt(i)
-                if (Math.random() <= 0.5) {
-                    letter = letter.toUpperCase()
-                    sufficiently_mocked = true
+            // !gamble
+            case 'gamble':
+                if (user == "KyloRennington") {
+                    // get beaned kyle
+                    bot.sendMessage({
+                        to: channelID,
+                        message: "you lose"
+                    });
+                } else {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: "you win"
+                    });
                 }
-                mocking_message += letter
-            }
-            if (mocking_message == "" || sufficiently_mocked == false) {
-                mocking_message = "fuck u Strodl Bot"
-            }
+            break;
+
+            // !northerngeese posts northen geese faces
+            case 'northerngeese':
+                bot.sendMessage({
+                    to: channelID,
+                    message: "${yetinate} ${james}: ${kaleb}"
+                });
+            break;
+
+            // !southerngeese posts southern geese faces
+            case 'southerngeese':
             bot.sendMessage({
                 to: channelID,
-                message: mocking_message
+                message: "${coolbob} ${bill} ${dan} ${jacob} ${jake} ${kelly} ${kyle} ${noah}"
             });
+        break;
         }
-    });
-}
+     }
 
-// lmao
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+     // Responds to "honk" with "honk". This is the most important feature.
+     if (message.toLowerCase().includes("honk") && user != "honkbot") {
+         bot.sendMessage({
+             to: channelID,
+             message: "honk"
+         });
+     }
+
+     // Also honkbot hanks now
+     if (message.toLowerCase().includes("hank") && user != "honkbot") {
+        bot.sendMessage({
+            to: channelID,
+            message: "hank"
+        });
+    }
+
+    /* Fuck Strodl Bot
+    Does the mocking spongebob text thing to whatever Strodl Bot says. I should extend this to include other users.
+    If there is no text to mock, simply tells Strodl Bot to fuck off. */
+     if (user == "Strodl Bot") {
+        let mocking_message = ""
+        let sufficiently_mocked = false
+        for (let i = 0; i < message.length; i++) {
+            letter = message.charAt(i)
+            if (Math.random() <= 0.5) {
+                letter = letter.toUpperCase()
+                sufficiently_mocked = true
+            }
+            mocking_message += letter
+        }
+        if (mocking_message == "" || sufficiently_mocked == false) {
+            mocking_message = "fuck u Strodl Bot"
+        }
+        bot.sendMessage({
+            to: channelID,
+            message: mocking_message
+        });
+     }
+});
