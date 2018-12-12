f2594d39fa097f6508390ea2a74b3718dee47c7e
Noah Kindervag <noahk@protonmail.com>
2018-12-11 18:45:23 -0600

    Update honkbot.js
    nope

--- honkbot.js
+++ honkbot.js
@@ -57,7 +57,6 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     say_message += args[i];
                     say_message += " "
                 }
-                message.deleteMessage(message.id);
                 bot.sendMessage({
                     to: channelID,
                     message: say_message
